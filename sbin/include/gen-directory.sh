0609bc2f9c1e22de567b1a0d2d25d97cbfb33e61
Artkoto <parfaitakoto@gmail.com>
2022-11-13 11:34:07 +0100

    :pencil2: naming convention updated

--- sbin/include/gen-directory.sh
+++ sbin/include/gen-directory.sh
@@ -7,17 +7,14 @@ function directoryGen(){
     do
         echo -n "# "
         read -r inputText
-        if [[ "$inputText" =~ ^([a-zA-Z][a-zA-Z0-9]{1,24})(\/[a-zA-Z][a-zA-Z0-9]{1,24})*$ ]]
+        if [[ "$inputText" =~ ^([a-zA-Z][a-zA-Z0-9]*)(\/[a-zA-Z][a-zA-Z0-9]*)*$ ]]
         then
             echo -e "\e[32m$inputText\e[39m"
-            echo ""
             break
         else
             echo -e "\e[31mFollow this naming convention:"
             echo "- start with an alphabetical character"
             echo "- Do not use a dash '-', underscore '_', or other characters ($, *, accents, ...)"
-            echo "- 2 characters minimum"
-            echo "- 30 characters maximum"
             echo "- use the separator '/'"
             echo -e "\e[39m"
         fi
